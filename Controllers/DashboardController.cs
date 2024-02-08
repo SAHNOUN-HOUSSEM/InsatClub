b64df842493f57b2348c16bdba9fe75a7f4bac9b
masmoudi aziz <azizmasmoudi019@gmail.com>
2024-02-09 00:10:51 +0100

    feat dashbord

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -10,6 +10,26 @@ namespace InsaClub.Controllers
     [Authorize]
     public class DashboardController : Controller
     {
-   
+        private readonly IDashboardRepository _dashboardRespository;
+        private readonly IPhotoService _photoService;
+
+        public DashboardController(IDashboardRepository dashboardRespository, IPhotoService photoService)
+        {
+            _dashboardRespository = dashboardRespository;
+            _photoService = photoService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // var userEvents = await _dashboardRespository.GetAllUserEvents();
+            var userEvents = new List<Event>();
+            var userClubs = await _dashboardRespository.GetAllUserClubs();
+            var dashboardViewModel = new DashboardViewModel()
+            {
+                Events = userEvents,
+                Clubs = userClubs
+            };
+            return View(dashboardViewModel);
+        }
     }
 }
